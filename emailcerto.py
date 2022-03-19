07fd6936316d1df7550351f0e5fc79851fb1f1d9
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2022-03-18 23:03:25 -0300

    Testar e-mail válido

---
+++ emailcerto.py
@@ -0,0 +1,10 @@
+#If pra testar se o e-mail está correto
+
+email_certo = 'leonardocherubino@gmail.com'
+meu_email = str(input('Entre com o seu e-mail: '))
+
+if meu_email != email_certo:
+    print('O e-mail está incorreto')
+else:
+    print('E-mail correto')
+    
